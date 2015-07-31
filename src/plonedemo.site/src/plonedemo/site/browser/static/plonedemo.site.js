42c095feec2e341e5d1421f71fef895f14bc41c6
Philip Bauer <bauer@starzel.de>
2015-07-31 12:48:24 +0200

    move js to template to make it work in modal

--- src/plonedemo.site/src/plonedemo/site/browser/static/plonedemo.site.js
+++ src/plonedemo.site/src/plonedemo/site/browser/static/plonedemo.site.js
@@ -1,14 +1,6 @@
+// The auto-login-code moved into
+// plonedemo/site/browser/overrides/Products.CMFPlone.skins.plone_login.login_form.cpt
 
 $(document).ready(function(){
-    $('a.autologin').on("click", function(e) {
-        e.preventDefault();
-        var username = $(this).attr('data-autologin-user');
-        var password = $(this).attr('data-autologin-password');
-        $('input#__ac_name').click();
-        $('input#__ac_name').val(username);
-        $('input#__ac_password').click();
-        $('input#__ac_password').val(password);
-        // $('.formControls input[type="submit"]').show();
-        $('.formControls input[type="submit"]').click();
-    });
+
 })
