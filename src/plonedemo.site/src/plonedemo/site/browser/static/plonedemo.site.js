27712e042d274c13e39f3a13baaf0d3cc1bbec6c
Philip Bauer <bauer@starzel.de>
2015-07-31 12:23:56 +0200

    enable autologin with js (TODO: does not work in modal)

--- src/plonedemo.site/src/plonedemo/site/browser/static/plonedemo.site.js
+++ src/plonedemo.site/src/plonedemo/site/browser/static/plonedemo.site.js
@@ -1,1 +1,14 @@
-// some js
+
+$(document).ready(function(){
+    $('a.autologin').on("click", function(e) {
+        e.preventDefault();
+        var username = $(this).attr('data-autologin-user');
+        var password = $(this).attr('data-autologin-password');
+        $('input#__ac_name').click();
+        $('input#__ac_name').val(username);
+        $('input#__ac_password').click();
+        $('input#__ac_password').val(password);
+        // $('.formControls input[type="submit"]').show();
+        $('.formControls input[type="submit"]').click();
+    });
+})
