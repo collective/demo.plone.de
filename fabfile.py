bdeb8a7bd49083ce2d749e50e397c8ce709546de
Stefan Antonelli <stefan.antonelli@operun.de>
2015-09-17 15:33:47 +0200

    updated fabfile

--- fabfile.py
+++ fabfile.py
@@ -11,11 +11,11 @@ from fabric.decorators import task
 env.use_ssh_config = True
 
 @task
-def demo(branch='master'):
+def demo_host(branch='master'):
     """
-    Demo Instance
+    Host serving our Plone demo
     """
-    env.hosts = ['demo.operun.de']
+    env.hosts = ['demo.plone.de']
     env.port = '30363'
     env.deploy_user = 'zope'
     env.branch = branch
@@ -46,9 +46,9 @@ def restart():
         sudo('./bin/supervisorctl restart all', user=env.deploy_user)
 
 @task
-def setup():
+def update():
     """
-    Clone buildout and setup plone
+    Update the instance and reinstall the demo
     """
     with cd(env.directory):
         
@@ -56,18 +56,15 @@ def setup():
         stop()
         
         # update plone
-        update()        
+        sudo('git pull', user=env.deploy_user)
+        sudo('git checkout {}'.format(env.branch), user=env.deploy_user)
         
         # bootstrap
         sudo('python bootstrap.py', user=env.deploy_user)
         
-        # delete .installed.cfg to get correct admin user from buildout's secret.cfg
+        # delete database and sone files
         sudo('rm -rf .installed.cfg', user=env.deploy_user)
-                
-        # remove blobs
         sudo('rm -rf ./var/blobstorage', user=env.deploy_user)
-        
-        # remove database
         sudo('rm -rf ./var/filestorage', user=env.deploy_user)        
 
         # buildout
@@ -75,29 +72,3 @@ def setup():
         
         # start zope
         start()
-
-
-def update():
-    """
-    Update packages
-    """
-    with cd(env.directory):
-        sudo('git pull', user=env.deploy_user)
-        sudo('git checkout {}'.format(env.branch), user=env.deploy_user)
-
-
-def buildout():
-    """
-    Run buildout
-    """
-    with cd(env.directory):
-        sudo('./bin/buildout', user=env.deploy_user)
-
-
-def deploy():
-    """
-    Deploy current status of development
-    """
-    update()
-    buildout()
-    restart()
