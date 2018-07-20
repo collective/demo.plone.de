f16aa9b8b02eef4e6df78687804ccc142759b218
Philip Bauer <bauer@starzel.de>
2018-07-20 13:55:47 +0200

    remove mention of version from static text

--- src/plonedemo.site/src/plonedemo/site/setuphandlers.py
+++ src/plonedemo.site/src/plonedemo/site/setuphandlers.py
@@ -19,7 +19,7 @@ import six
 logger = logging.getLogger(__name__)
 
 DEFAULT_EMAIL = 'demo@plone.de'
-FRONTPAGE_TITLE = _(u'Welcome to Plone 5.1')
+FRONTPAGE_TITLE = _(u'Welcome to Plone')
 FRONTPAGE_DESCRIPTION = _('The ultimate Open Source Enterprise CMS')
 IMPORTED_FOLDER_ID = 'demo'
 
