2fbddc89c741b37aa33b9cf7691122c9fdd44f93
Philip Bauer <bauer@starzel.de>
2018-07-20 13:36:21 +0200

    fix flake8

--- src/plonedemo.site/src/plonedemo/site/browser/viewlets.py
+++ src/plonedemo.site/src/plonedemo/site/browser/viewlets.py
@@ -29,5 +29,6 @@ class VersionsViewlet(ViewletBase):
 
     def version_overview(self):
         portal = api.portal.get()
-        view = api.content.get_view('overview-controlpanel', portal, self.request)
+        view = api.content.get_view(
+            'overview-controlpanel', portal, self.request)
         return view.version_overview()
