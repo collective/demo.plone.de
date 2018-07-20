6f77df35342121122ee991313ce70f682d58c993
Philip Bauer <bauer@starzel.de>
2018-07-20 10:49:44 +0200

    show versions in a viewlet

--- src/plonedemo.site/src/plonedemo/site/browser/viewlets.py
+++ src/plonedemo.site/src/plonedemo/site/browser/viewlets.py
@@ -4,6 +4,7 @@ from plone.app.layout.viewlets.common import ViewletBase
 from plone.app.layout.navigation.interfaces import INavigationRoot
 
 import pkg_resources
+import six
 
 
 class FrontpageViewlet(ViewletBase):
@@ -16,7 +17,17 @@ class FrontpageViewlet(ViewletBase):
             return context_state.is_view_template()
 
     def get_plone_version(self):
+        if six.PY3:
+            return '5.2 with Python 3.6'
         try:
             return pkg_resources.get_distribution('Plone').version
         except pkg_resources.DistributionNotFound:
             return pkg_resources.get_distribution('Products.CMFPlone').version
+
+
+class VersionsViewlet(ViewletBase):
+
+    def version_overview(self):
+        portal = api.portal.get()
+        view = api.content.get_view('overview-controlpanel', portal, self.request)
+        return view.version_overview()
